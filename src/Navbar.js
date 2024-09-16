4f3a966a87bcdf41acaf3baa4625ffd76d6d9ee4
https://github.com/AliOsmanUnaldi <aliunaldizcb@gmail.com>
2024-09-16 19:08:41 +0300

    add hamburger menu

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,21 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <h1>Dyt. Emin Tatlılıoğlu</h1>
-      <ul>
+      <button className="hamburger" onClick={toggleMenu}>
+        {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+      </button>
+      <ul className={`nav-menu ${isOpen ? 'active' : ''}`}>
         <li><Link to="/dietitian/">Ana Sayfa</Link></li>
         <li><Link to="/dietitian/recipes">Tarifler</Link></li>
         <li><Link to="/dietitian/blogs">Bloglar</Link></li>
